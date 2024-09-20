6fc51589c0d260cb2f3f8c370e79001937053ad0
Carson Andorf <andorfc@iastate.edu>
2024-09-20 11:18:54 -0500

    Manuscript release - support for NAM

--- SNPversity/processForm.php
+++ SNPversity/processForm.php
@@ -19,17 +19,29 @@ if (!is_numeric($start) || !is_numeric($end)) {
 
 //$db = escapeshellarg("./hdf5/chr10_test.h5");
 
+$ds_part0 = "maizegdb2024";
 $ds_part1 = $chr;
 $ds_part2 = "HQ";
 
-if($dataset == "mgdb2024_hq")
-{
-        $ds_part2 = "HQ";
-} else {
-        $ds_part2 = "HC";
+if ($dataset == "mgdb2024_hq") {
+    $ds_part0 = "maizegdb2024";
+    $ds_part2 = "HQ";
+} else if ($dataset == "mgdb2024_hc") {  // Fix the condition here
+    $ds_part0 = "maizegdb2024";
+    $ds_part2 = "HC";
+} else if ($dataset == "schnable2023") {
+    $ds_part0 = "schnable2023";
+    $ds_part2 = "impute";
+} else if ($dataset == "nam2021_hq") {
+    $ds_part0 = "nam2021";
+    $ds_part2 = "HQ";
+} else if ($dataset == "nam2021_hc") {
+    $ds_part0 = "nam2021";
+    $ds_part2 = "HC";
 }
 
-$db_filename = "./hdf5/maizegdb2024_" . $ds_part1 . "_" . $ds_part2 . ".h5";
+//$db_filename = "./hdf5/maizegdb2024_" . $ds_part1 . "_" . $ds_part2 . ".h5";
+$db_filename = "./hdf5/" . $ds_part0 . "_" . $ds_part1 . "_" . $ds_part2 . ".h5";
 //$db_filename = "./hdf5/" . $ds_part1 . "_" . $ds_part2 . ".h5";
 
 //$db = escapeshellarg("./hdf5/chr10_HQ_small.h5");
@@ -52,7 +64,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $output = shell_exec($command);
 
     if ($output === null) {
-        echo json_encode(array("status" => "error", "message" => "Python script execution failed"));
+        echo json_encode(array("status" => "error", "message" => "Python script execution failed "));
+        //echo json_encode(array("status" => "success", "message" => "$command" ));
     } else {
         echo json_encode(array("status" => "success", "message" => "Python script executed", "output" => $output));
     }
